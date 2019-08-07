63a1596a90416c3293441caf003afbc4aefab6c5
esjeong <esjeong@solidware.io>
2019-08-07 17:58:04 +0900

    Fix bug

--- dqn.py
+++ dqn.py
@@ -59,12 +59,11 @@ class DQNAgent:
         return random.sample(self.replay_memory, min(size, len(self.replay_memory)))
 
     def predict(self, state, target_model=False):
-        X = np.reshape(state, [len(state), self.state_space[0]])
+        X = np.reshape(state, [1, self.state_space[0]])
         if target_model:
             return self.target_model.predict(X)
         else:
             return self.model.predict(X)
-
 
     def copy_model_to_target_model(self):
         self.target_model.set_weights(self.model.get_weights())
@@ -95,30 +94,30 @@ class Env:
         self.total_episode_count = 0
 
     def decide_action(self, state):
-        e = 1. / ((self.total_episode_count / 10) + 1)
+        e = 1. / ((self.total_episode_count / 10_000) + 1)
         if np.random.rand(1) < e:
             return self.env.action_space.sample()
         else:
             return np.argmax(self.model.predict(state))
 
     def run(self, n_times = 100):
-        action_count = 0
-        action_counts = []
+        total_action_counts = []
         for _ in tqdm(range(n_times)):
             done = False 
-            action_count = 0
+            episode_action_count = 0
             state = self.env.reset()
             while not done:
                 action = self.decide_action(state)
                 next_state, reward, done, _ = self.env.step(action)
-                action_count += 1
-                reward = self.get_reward(action_count, done)
-                self.model.update_memory((state, action, reward, next_state, done))
+                episode_action_count += 1
+                reward = self.get_reward(episode_action_count, done)
+                self.model.update_memory((state.tolist(), action, reward, next_state, done))
                 self.model.train_from_memory()
                 state = next_state
-            action_counts.append(action_count)
+            total_action_counts.append(episode_action_count)
             self.model.copy_model_to_target_model()
-        print("Average action count : {}".format(sum(action_counts)/n_times))
+        print("Average action count : {}".format(sum(total_action_counts)/n_times))
+        self.total_episode_count += sum(total_action_counts)
 
     def get_reward(self, action_count, done):
         if done and action_count < 199:
@@ -128,20 +127,42 @@ class Env:
         else:
             return 0
 
-    def test(self, save_image_path = 'test.gif'):
-        state = self.env.reset()
+    def test(self, save_image_path = 'test.gif', n=100):
+        # Test 100 episodes
+        total_episode_images = []
         episode_image = []
-        action_count = 0
-        while True:
-            episode_image.append(self.env.render(mode='rgb_array'))
-            action = np.argmax(self.model.predict(state))
-            next_state, _, done, _ = self.env.step(action)
-            action_count += 1
-            state = next_state
-            if done:
-                imageio.mimsave(save_image_path, episode_image, duration=0.05)
-                print("Test action count :", str(action_count))
-                break
+        total_action_counts = []
+        # Save 5 trial
+        for _ in range(min(5, n)):
+            single_action_count = 0
+            state = self.env.reset()
+            while True:
+                episode_image.append(self.env.render(mode='rgb_array'))
+                action = np.argmax(self.model.predict(state))
+                next_state, _, done, _ = self.env.step(action)
+                single_action_count += 1
+                state = next_state
+                if done:
+                    total_episode_images.extend(episode_image)
+                    total_action_counts.append(single_action_count)
+                    episode_image = []
+                    break
+        self.env.close()
+        imageio.mimsave(save_image_path, total_episode_images, duration=0.05)
+
+        for _ in range(n-5):
+            single_action_count = 0
+            state = self.env.reset()
+            while True:
+                action = np.argmax(self.model.predict(state))
+                next_state, _, done, _ = self.env.step(action)
+                single_action_count += 1
+                state = next_state
+                if done:
+                    total_action_counts.append(single_action_count)
+                    break
+        print("Test average action count {} times :".format(n), str(sum(total_action_counts)/n))
+
 
 if __name__ == '__main__':
     image_folder_path = 'test_gifs'
@@ -150,8 +171,4 @@ if __name__ == '__main__':
     env = Env()
     for i in range(10):
         env.run(100)
-        env.test(os.path.join(image_folder_path, 'test_'+str(i)+'.gif'))
-
-    # def save_episode_image(self):
-    #     save_episode = list(range(len(self.total_episode_count) // 10)
-    #     pass
+        env.test(os.path.join(image_folder_path, 'test_'+str(i)+'.gif'), n=10)
